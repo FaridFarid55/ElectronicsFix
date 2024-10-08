3bf383c5d8b23dfd392ded6909c38961fcc06e2b
Menna Hassan <h.menna85@gmail.com>
2024-10-08 11:01:26 +0300

    added the crud operations and the controller using scaffolding approach

---
+++ ElectronicsFix/Controllers/EngineersController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bl;
+using Domains;
+
+namespace ElectronicsFix.Controllers
+{
+    public class EngineersController : Controller
+    {
+        private readonly DepiProjectContext _context;
+
+        public EngineersController(DepiProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Engineers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Engineers.ToListAsync());
+        }
+
+        // GET: Engineers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var engineer = await _context.Engineers
+                .FirstOrDefaultAsync(m => m.EngineerId == id);
+            if (engineer == null)
+            {
+                return NotFound();
+            }
+
+            return View(engineer);
+        }
+
+        // GET: Engineers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Engineers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EngineerId,FirstName,LastName,Phone,Address,Email,Password,ConfirmPassword")] Engineer engineer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(engineer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(engineer);
+        }
+
+        // GET: Engineers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var engineer = await _context.Engineers.FindAsync(id);
+            if (engineer == null)
+            {
+                return NotFound();
+            }
+            return View(engineer);
+        }
+
+        // POST: Engineers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EngineerId,FirstName,LastName,Phone,Address,Email,Password,ConfirmPassword")] Engineer engineer)
+        {
+            if (id != engineer.EngineerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(engineer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EngineerExists(engineer.EngineerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(engineer);
+        }
+
+        // GET: Engineers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var engineer = await _context.Engineers
+                .FirstOrDefaultAsync(m => m.EngineerId == id);
+            if (engineer == null)
+            {
+                return NotFound();
+            }
+
+            return View(engineer);
+        }
+
+        // POST: Engineers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var engineer = await _context.Engineers.FindAsync(id);
+            if (engineer != null)
+            {
+                _context.Engineers.Remove(engineer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EngineerExists(int id)
+        {
+            return _context.Engineers.Any(e => e.EngineerId == id);
+        }
+    }
+}
