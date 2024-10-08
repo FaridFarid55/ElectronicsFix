28604ec76879cd1dcdbb770bd5d88d20923e453a
Menna Hassan <h.menna85@gmail.com>
2024-10-08 10:46:56 +0300

    Add data annotation to the engineer class

--- Domains/Engineer.cs
+++ Domains/Engineer.cs
@@ -1,24 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domains;
 
 public partial class Engineer
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int EngineerId { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public string FirstName { get; set; } = null!;
 
+    [Required]
+    [StringLength(100)]
     public string LastName { get; set; } = null!;
 
+    [StringLength(15)]
     public string? Phone { get; set; }
 
+    [StringLength(255)]
     public string? Address { get; set; }
 
+    [Required]
+    [EmailAddress]
+    [StringLength(100)]
     public string Email { get; set; } = null!;
 
+    [Required]
+    [StringLength(100)]
     public string Password { get; set; } = null!;
 
+    [Required]
+    [StringLength(100)]
     public string ConfirmPassword { get; set; } = null!;
 
     public virtual ICollection<Consultation> Consultations { get; set; } = new List<Consultation>();
