204b357f6639eeeb475df94b1897479d88360b3c
Farid Farid <faridfaridfr5@gmail.come>
2024-10-16 16:25:26 +0300

    /

--- Global/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Bl;
-global using Domains;
-global using ElectronicsFix.Ui;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
-
-
--- RazorClassLibrary1/Global/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿//global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-//global using Microsoft.AspNetCore.Identity;
-//global using Microsoft.EntityFrameworkCore.Migrations;
-//global using ElectronicsFix.Models;
-global using Domains;
-global using Microsoft.EntityFrameworkCore;
-
