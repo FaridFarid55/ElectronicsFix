694790b95e0e0cd92d06965af168ae1f70cc7afd
Farid Farid <faridfaridfr5@gmail.come>
2024-10-20 19:09:23 +0300

    x

--- ElectronicsFix/Controllers/EngineerTaskController.cs
+++ ElectronicsFix/Controllers/EngineerTaskController.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Bl;
-using Domains;
-
-namespace ElectronicsFix.Controllers
+﻿namespace ElectronicsFix.Controllers
 {
     public class EngineersController : Controller
     {
